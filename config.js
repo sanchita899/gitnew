9e61ba80a731bb8294646462fa29094fecd8bd0b
sanchita <sanchitayadav709@gmail.com>
2025-05-25 12:56:50 +0530

    version 2

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 console.log('config.js');
 console.log('version190000');
+console.log('hello');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('index.js');
 console.log('version newest yet again');
+console.log('version 10');
